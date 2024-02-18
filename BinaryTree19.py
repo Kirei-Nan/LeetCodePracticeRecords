feada3cca0adf520cbbe73abaf248ba5a89525b6
Kirei-Nan <nanzhenghan@outlook.com>
2024-02-17 21:30:50 -0500

    BinaryTree19.py:第一遍不会，看了答案才写的

---
+++ BinaryTree19.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def constructMaximumBinaryTree(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if len(nums) == 1:
+            return TreeNode(nums[0])
+        max_val=0
+        max_index=0
+        if nums:
+            max_val=max(nums)
+            max_index = nums.index(max_val)
+            root = TreeNode(max_val)
+            root.left = self.constructMaximumBinaryTree(nums[:max_index])
+            root.right = self.constructMaximumBinaryTree(nums[max_index + 1:])
+            return root
