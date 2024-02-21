13c1a1d26e75051f86d44d348fa76900a6f03af3
Kirei-Nan <nanzhenghan@outlook.com>
2024-02-21 13:51:36 -0500

    BackTrack2.py:不会第一遍写的时候

---
+++ BackTrack2.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def backtrack(self,n,k,startIndex,path,result):
+        if len(path)==k:
+            result.append(path[:])
+            return result
+        for i in range(startIndex,n+1):
+            path.append(i)
+            self.backtrack(n,k,i+1,path,result)
+            path.pop()
+
+
+    def combine(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        result=[]
+        self.backtrack(n, k, 1, [],result)
+        return result
+
