c599ce453970fa1902771cd35772634cbae3b8f1
Kirei-Nan <nanzhenghan@outlook.com>
2024-02-27 16:37:19 -0500

    BackTrack14.py:第一遍自己就写出来了,虽然通过了，但是其实是错的，因为test case里是按大小排列的，如果遇到没排列的情况就错了。这里要用set去重，因为不能对nums排列

--- BackTrack14.py
+++ BackTrack14.py
@@ -1,35 +1,22 @@
 class Solution(object):
-    def isValid(self,path):
-        if len(path)<2:
-            return False
-        prev=None
-        for i in path:
-            if prev is None:
-                prev=i
-            else:
-                if i<prev:
-                    return False
-        return True
-    def backtrack(self,nums,path,result,startIndex,used):
-        if self.isValid(path):
+    def backtrack(self, nums, path, result,startIndex):
+        if len(path)>1:
             result.append(path[:])
-
+        uset=set()
         for i in range(startIndex,len(nums)):
-            if i>0 and used[i-1]==False and nums[i]==nums[i-1]:
+            if ( path and nums[i]<path[-1]) or nums[i] in uset:
                 continue
-            path.append(nums[i])
-            used[i]=True
-            self.backtrack(nums,path,result,i+1,used)
-            path.pop()
-            used[i]=False
 
+            uset.add(nums[i])
+            path.append(nums[i])
+            self.backtrack(nums, path, result,i+1)
+            path.pop()
 
     def findSubsequences(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        used=[False]*len(nums)
         result=[]
-        self.backtrack(nums,[],result,0,used)
+        self.backtrack(nums, [],result,0)
         return result
