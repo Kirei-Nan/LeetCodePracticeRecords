b6c55ac35114e9b3e3f079773f9c4f3d4fc2a039
Kirei-Nan <nanzhenghan@outlook.com>
2024-03-09 15:17:21 -0500

    DP8.py:简单题，但是第一遍不会

---
+++ DP8.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def integerBreak(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp=[0 for i in range(n+1)]
+        dp[0]=0
+        dp[1]=0
+        dp[2]=1
+        for i in range(3,n+1):
+            for j in range(1,i):
+                dp[i]=max(j*(i-j),j*dp[i-j],dp[i])
+        return dp[n]
