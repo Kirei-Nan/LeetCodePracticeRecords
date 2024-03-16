6f611e0dfa9ae85459b20039601545df3c54715e
Kirei-Nan <nanzhenghan@outlook.com>
2024-03-15 21:05:48 -0400

    LinkedList2.py:这里原来没加else，这样是错的，因为这样会导致比如cur到最后一个节点会使得cur变成null

---
+++ LinkedList2.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def removeElements(self, head, val):
+        """
+        :type head: ListNode
+        :type val: int
+        :rtype: ListNode
+        """
+        newhead=ListNode(0,head)
+        cur=newhead
+        while cur.next:
+            if cur.next.val==val:
+                cur.next=cur.next.next
+            else:
+                cur=cur.next
+        return newhead.next
