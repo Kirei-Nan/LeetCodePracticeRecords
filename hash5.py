a61fd7f003a90e2db1d925e48b452f69fcfaca49
Kirei-Nan <nanzhenghan@outlook.com>
2024-03-16 21:03:48 -0400

    hash5.py:第一遍做不会

--- hash5.py
+++ hash5.py
@@ -5,6 +5,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+
         records={}
         for i in range(len(nums)):
             if target-nums[i] in records:
