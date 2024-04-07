57a636653d8c7296fbdaf14d2b755d6b926d40b8
Kirei-Nan <nanzhenghan@outlook.com>
2024-04-06 22:16:08 -0400

    第一遍不会。注意不能写成dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i])因为这样就会导致不止是在一次交易下的情况。会导致买入，卖出再买入

---
+++ DP32.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        length=len(prices)
+        if len == 0:
+            return 0
+
+        dp=[[0]*2 for _ in range(length)]
+        dp[0][0] = -prices[0]#持有股票
+        dp[0][1] = 0#不持有股票
+
+        for i in range(1, length):
+            dp[i][0]=max(dp[i-1][0],-prices[i])
+            dp[i][1]=max(dp[i-1][1],dp[i-1][0]+prices[i])
+        return dp[-1][1]
