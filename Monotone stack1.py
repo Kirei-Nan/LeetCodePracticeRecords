316d769d170e5e520aa7e8e4fa352bba90b5b7c6
Kirei-Nan <nanzhenghan@outlook.com>
2024-06-08 11:29:11 +0800

    第一遍不会，是难题

---
+++ Monotone stack1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer=[0]*len(temperatures)
+        stack=[0]
+
+        for i in range(1,len(temperatures)):
+            if temperatures[i]<=temperatures[stack.top()]:
+                stack.append(temperatures[i])
+            else:
+                while len(stack)!=0 and temperatures[i]>temperatures[stack.top()]:
+                    answer[stack[-1]]=i-stack[-1]
+                    stack.pop()
+                stack.append(i)
+        return answer
