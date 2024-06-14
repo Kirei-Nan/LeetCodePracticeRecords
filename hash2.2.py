5ba6968631f39952954cc7bddb6bfe74ed8ba3d1
Kirei-Nan <nanzhenghan@outlook.com>
2024-06-14 13:37:40 +0800

    没过

---
+++ hash2.2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        result=[0]*26
+        for i in s:
+            result[ord(i)-ord("a")]+=1
+        for i in t:
+            result[ord(i) - ord("a")] -=1
+        for i in range(26):
+            if result[i]!=0:
+                return False
+        return True
