208c40d4cbb2a3843e99adac35dc1f6f197f75be
Kirei-Nan <nanzhenghan@outlook.com>
2024-06-23 10:06:36 +0800

    难题

---
+++ Stack&Queue3.py
@@ -0,0 +1,34 @@
+class MyStack:
+
+    def __init__(self):
+        self.queue_in=[]
+        self.queue_out=[]
+
+    def push(self, x: int) -> None:
+        self.queue_in.append(x)
+
+    def pop(self) -> int:
+        if self.empty():
+            return None
+        for i in range(len(self.queue_in)-1):
+            self.queue_out.append(self.queue_in.pop(0))
+        self.queue_in,self.queue_out=self.queue_out,self.queue_in
+        return self.queue_out.pop(0)
+
+
+    def top(self) -> int:
+        if self.empty():
+            return None
+        return self.queue_in[-1]
+
+
+    def empty(self) -> bool:
+        return len(self.queue_in)==0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
