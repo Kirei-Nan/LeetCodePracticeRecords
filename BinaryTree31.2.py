b8c79fbefd4ba0cb91eef9e5e9f6d8e0c99e1e68
Kirei-Nan <nanzhenghan@outlook.com>
2024-07-03 12:23:07 +0800

    难题，考验return得很好的题

---
+++ BinaryTree31.2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def trimBST(self, root: TreeNode, low: int, high: int) -> TreeNode:
+        if root is None:
+            return None
+        if root.val < low:
+            # 寻找符合区间 [low, high] 的节点
+            return self.trimBST(root.right, low, high)
+        if root.val > high:
+            # 寻找符合区间 [low, high] 的节点
+            return self.trimBST(root.left, low, high)
+        root.left = self.trimBST(root.left, low, high)  # root.left 接入符合条件的左孩子
+        root.right = self.trimBST(root.right, low, high)  # root.right 接入符合条件的右孩子
+        return root
