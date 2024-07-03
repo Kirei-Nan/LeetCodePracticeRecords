743d9b6b190aa90fb41688f023c034be2c15c516
Kirei-Nan <nanzhenghan@outlook.com>
2024-07-03 13:04:10 +0800

    简单题，但是注意中节点的计算

---
+++ BinaryTree32.2.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if len(nums)==0:
+            return None
+        left=0
+        right=len(nums)-1
+        mid=left+(right-left)//2
+        root=TreeNode(nums[mid])
+        root.left=self.sortedArrayToBST(nums[:mid])
+        root.right=self.sortedArrayToBST(nums[mid+1:len(nums)])
+        return root
