d74d46c648828f31341f1817c8c12cfd3d665c05
Kirei-Nan <nanzhenghan@outlook.com>
2024-07-15 12:47:29 +0800

    难题

---
+++ Greedy3.3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        prediff=0
+        curdiff=0
+        result=1
+        for i in range(1,len(nums)):
+            curdiff=nums[i]-nums[i-1]
+            if prediff>=0 and curdiff<0 or prediff<=0 and curdiff>0:
+                result+=1
+                prediff=curdiff
+        return result
