04aeb683b6a70b13e2c1a2c69b9fad3c7da463fd
Kirei-Nan <nanzhenghan@outlook.com>
2024-07-25 08:11:39 +0800

    难题，注意else的情况也就是dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])

---
+++ DP53.2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        dp = [[0] * len(s) for _ in range(len(s))]
+
+        for i in range(len(s) - 1, -1, -1):
+            for j in range(i, len(s)):
+                if s[i] == s[j]:
+                    if j - i < 1:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        return dp[0][-1]
