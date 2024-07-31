b8376eb8f454f73faad68e16c68440c54b2184f4
Kirei-Nan <nanzhenghan@outlook.com>
2024-07-31 12:22:58 +0800

    pass

---
+++ String4.2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        res=s.strip().split()
+        left=0
+        right=len(res)-1
+        while left<right:
+            res[left],res[right]=res[right],res[left]
+            left+=1
+            right-=1
+        return " ".join(res)
